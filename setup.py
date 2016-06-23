e3fedacaf7ae8c3d7affe78f8301581416bb844a
Brandon Cazander <brandon.cazander@multapplied.net>
2016-06-23 11:45:32 -0700

    Add setuptools.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='python-ipset',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/Lukasa/python-ipset',
+    license='GPLv2',
+    author='Cory Benfield',
+    author_email='cory@lukasa.co.uk',
+    description='A Python wrapper around libipset.'
+)
